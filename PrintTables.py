9a0c7a22b00e9d45367ecdf3899273df37fdff83
Sumanth3211 <Sumanthgotham14@gmail.com>
2025-02-08 10:18:08 +0530

    Using while and for

--- PrintTables.py
+++ PrintTables.py
@@ -3,5 +3,16 @@ End=20
 for i in range(start,End):
     for j in range (1,10):
         print (f"{i}*{j}={i*j}")
-        print("----------------------")
+    
+    print("----------------------")
         
+start = 1
+end = 20
+
+while start <= end:
+    cont = 1  
+    while cont <= 10:
+        print(f"{start} * {cont} = {start * cont}")
+        cont += 1
+    print("----------------------") 
+    start += 1
