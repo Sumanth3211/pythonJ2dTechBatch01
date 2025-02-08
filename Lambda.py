148a2a8d0df175b400b64763c39e33829f5d1211
Sumanth3211 <Sumanthgotham14@gmail.com>
2025-02-08 10:05:06 +0530

    Tables

--- Lambda.py
+++ Lambda.py
@@ -3,4 +3,3 @@ my_List=[1,2,3,4,5]
 squareRoot=list(map(lambda z:z**2, my_List))
 
 print(squareRoot)
-
---
+++ PrintTables.py
@@ -0,0 +1,7 @@
+start=1
+End=20
+for i in range(start,End):
+    for j in range (1,10):
+        print(f"{i}*{j}={i*j}):
+        print("----------------------"):
+        
