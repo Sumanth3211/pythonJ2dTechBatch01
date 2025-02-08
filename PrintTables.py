d6d07c316a12e687d66edfafa59b75a32b5f2d60
Sumanth3211 <Sumanthgotham14@gmail.com>
2025-02-08 10:11:09 +0530

    Printtables

--- PrintTables.py
+++ PrintTables.py
@@ -2,6 +2,6 @@ start=1
 End=20
 for i in range(start,End):
     for j in range (1,10):
-        print(f"{i}*{j}={i*j}):
-        print("----------------------"):
+        print (f"{i}*{j}={i*j}")
+        print("----------------------")
         
